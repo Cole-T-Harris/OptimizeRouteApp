c8419f465e740f2d79448a7e45beb6587acf3354
Cole Harris <cole.t.harr@gmail.com>
2024-08-09 17:53:05 -0600

    Cleaning up and reformatting terraform

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,32 +3,32 @@ provider "aws" {
 }
 
 module "optimize_route_function" {
-  source            = "./lambda_module"
-  function_name     = "optimize_route_function"
-  handler           = "handler1"
-  runtime           = "provided.al2023"
-  filename          = "../dist/optimizeRoute/optimizeRoute.zip"
+  source        = "./modules/lambda"
+  function_name = "optimize_route_function"
+  handler       = "handler1"
+  runtime       = "provided.al2023"
+  filename      = "../dist/optimizeRoute/optimizeRoute.zip"
   environment_variables = {
-    GOOGLE_API_KEY: var.GOOGLE_API_KEY
-    SUPABASE_URL: var.SUPABASE_URL
-    SUPABASE_KEY: var.SUPABASE_KEY  
+    GOOGLE_API_KEY : var.GOOGLE_API_KEY
+    SUPABASE_URL : var.SUPABASE_URL
+    SUPABASE_KEY : var.SUPABASE_KEY
   }
-  lambda_timeout    = 15
+  lambda_timeout = 15
 }
 
 module "commutes_queue_function" {
-  source            = "./lambda_module"
-  function_name     = "commutes_queue_function"
-  handler           = "handler1"
-  runtime           = "provided.al2023"
-  filename          = "../dist/commutesQueue/commutesQueue.zip"
+  source        = "./modules/lambda"
+  function_name = "commutes_queue_function"
+  handler       = "handler1"
+  runtime       = "provided.al2023"
+  filename      = "../dist/commutesQueue/commutesQueue.zip"
   environment_variables = {
-    SUPABASE_USERNAME: var.SUPABASE_USERNAME
-    SUPABASE_PASSWORD: var.SUPABASE_PASSWORD
-    SUPABASE_HOST: var.SUPABASE_HOST
-    SUPABASE_PORT: var.SUPABASE_PORT
-    SUPABASE_DATABASE: var.SUPABASE_DATABASE
-    OPTIMIZE_ROUTE_FUNCTION: module.optimize_route_function.function_arn
+    SUPABASE_USERNAME : var.SUPABASE_USERNAME
+    SUPABASE_PASSWORD : var.SUPABASE_PASSWORD
+    SUPABASE_HOST : var.SUPABASE_HOST
+    SUPABASE_PORT : var.SUPABASE_PORT
+    SUPABASE_DATABASE : var.SUPABASE_DATABASE
+    OPTIMIZE_ROUTE_FUNCTION : module.optimize_route_function.function_arn
   }
-    lambda_timeout    = 180
+  lambda_timeout = 180
 }
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,40 +1,40 @@
 variable "GOOGLE_API_KEY" {
-    description = "Google Route API Key"
-    sensitive   = true
-    type        = string
+  description = "Google Route API Key"
+  sensitive   = true
+  type        = string
 }
 variable "SUPABASE_URL" {
-    description = "Supabase Database URL"
-    sensitive   = true
-    type        = string
+  description = "Supabase Database URL"
+  sensitive   = true
+  type        = string
 }
 variable "SUPABASE_KEY" {
-    description = "Supabase Database Key"
-    sensitive   = true
-    type        = string
+  description = "Supabase Database Key"
+  sensitive   = true
+  type        = string
 }
 variable "SUPABASE_USERNAME" {
-    description = "Supabase Username"
-    sensitive   = true
-    type        = string
+  description = "Supabase Username"
+  sensitive   = true
+  type        = string
 }
 variable "SUPABASE_PASSWORD" {
-    description = "Supabase Password"
-    sensitive   = true
-    type        = string
+  description = "Supabase Password"
+  sensitive   = true
+  type        = string
 }
 variable "SUPABASE_HOST" {
-    description = "Supabase Host Name"
-    sensitive   = true
-    type        = string
+  description = "Supabase Host Name"
+  sensitive   = true
+  type        = string
 }
 variable "SUPABASE_PORT" {
-    description = "Supabase Host Name"
-    sensitive   = true
-    type        = string
+  description = "Supabase Host Name"
+  sensitive   = true
+  type        = string
 }
 variable "SUPABASE_DATABASE" {
-    description = "Supabase Database"
-    sensitive   = true
-    type        = string
+  description = "Supabase Database"
+  sensitive   = true
+  type        = string
 }
